06a86d873ce1e4a9ed72e92aff7fedffc32c2bd8
오승훈 <120100354+os-hoon@users.noreply.github.com>
2024-06-19 14:42:19 +0900

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
